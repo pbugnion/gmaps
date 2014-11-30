87c02c8e049fba43b967c410605a8595747444c4
Pascal Bugnion <pascal@bugnion.org>
2014-11-30 12:55:39 +0000

    Added loader script.
    The loader script contains an init function that loads the Googlemaps
    api and all the libraries.
    
    This should really only be seen as a stop-gap solution. The main problem
    with this (apart from forcing the user to call an init function) is that
    if the user resets the kernel and re-calls the init function, the JS
    will be injected into the DOM more than once.

---
+++ gmaps/loader.py
@@ -0,0 +1,29 @@
+
+from IPython.display import Javascript, display
+import os
+import sys
+
+__has_initialized__ = False
+
+js_libnames = ["heatmap_view.js"]
+
+def init():
+    global __has_initialized__
+    if not __has_initialized__:
+        js = """
+        window.gmap_initialize = function() {};
+        $.getScript('https://maps.googleapis.com/maps/api/js?v=3&sensor=false&libraries=visualization&callback=gmap_initialize');
+        """
+        __has_initialized__ = True
+        display(Javascript(data=js))
+        _load_jslibs()
+
+def _load_jslibs():
+    # Maybe this should be refactored to use nbextensions?
+    self_module = sys.modules[__name__]
+    self_path = os.path.dirname(self_module.__file__)
+    path_to_libs = os.path.join(self_path, "js")
+    for libname in js_libnames:
+        with open(os.path.join(path_to_libs, libname)) as f:
+            display(Javascript(f.read()))
+
