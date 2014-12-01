eff6c397a985c4f124fc1a8532a480532dc28013
Pascal Bugnion <pascal@bugnion.org>
2014-12-01 15:43:34 +0000

    Tidied loader.py and added information about hack.

--- gmaps/loader.py
+++ gmaps/loader.py
@@ -1,13 +1,16 @@
 
 from IPython.display import Javascript, display
-import os
-import sys
 
 __has_initialized__ = False
 
-js_libnames = ["heatmap_view.js"]
-
 def init():
+    # FIXME
+    # Loads the JS extensions. This is still a bit of a 
+    # hack: calls to gmaps methods cannot occur in the 
+    # same cell as the import statement. 
+    #
+    # Potential alternatives would be to re-factor the 
+    # init statement into a line magic.
     global __has_initialized__
     if not __has_initialized__:
         display(Javascript("""
