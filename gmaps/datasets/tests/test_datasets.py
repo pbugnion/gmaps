d371ce032625320b8c265c6bb54632d10fca64a7
Pascal Bugnion <pascal@bugnion.org>
2016-03-26 07:52:35 +0000

    Adapt tests to not use numpy

--- gmaps/datasets/tests/test_datasets.py
+++ gmaps/datasets/tests/test_datasets.py
@@ -8,7 +8,7 @@ class TestDatasets(unittest.TestCase):
 	def test_list_datasets(self):
 		assert 'taxi_rides' in datasets.list_datasets()
 
-	def test_load_datasets(sefl):
+	def test_load_datasets(self):
 		data = datasets.load_dataset('taxi_rides')
-		assert data.shape == (500,)
-		assert data.dtype.names == ('Latitude', 'Longitude')
+		assert data[0] == [37.782551, -122.445368]
+		assert len(data) == 500
