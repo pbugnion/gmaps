63f2d2c5a8b7251c518777f00ce6d0fd9e88c737
Pascal Bugnion <pascal@bugnion.org>
2016-07-26 05:40:33 +0100

    Docstring for NullableList traitlet.

--- gmaps/nullable_containers.py
+++ gmaps/nullable_containers.py
@@ -4,6 +4,13 @@ import sys
 import traitlets
 
 class NullableList(traitlets.List):
+    """
+    Traitlet for an attribute that can either be a list or None
+
+    This is different from List() inasmuch as List(default_value=None)
+    defaults to an empty list, whereas NullableList(default_value=None)
+    defaults to None.
+    """
 
     def __init__(self, trait=None, default_value=None, minlen=0, maxlen=sys.maxsize, **metadata):
         super(NullableList, self).__init__(
