fa1d309a509bd6e474081b74c8fbb3e5c10428a4
Pascal Bugnion <pascal@bugnion.org>
2016-07-28 07:30:30 +0100

    Test options mixin

---
+++ gmaps/tests/test_maps.py
@@ -0,0 +1,20 @@
+
+import unittest
+
+import gmaps
+
+class TestHeatmapOptionsMixin(unittest.TestCase):
+
+    def test_gradient_default_none(self):
+        l = gmaps.maps._HeatmapOptionsMixin()
+        assert l.gradient is None
+
+    def test_gradient_default_values(self):
+        l = gmaps.maps._HeatmapOptionsMixin(gradient=["blue", "red"])
+        assert l.gradient == ["blue", "red"]
+
+    def test_gradient_set_none(self):
+        l = gmaps.maps._HeatmapOptionsMixin(gradient=["blue", "red"])
+        l.gradient = None
+        assert l.gradient is None
+
