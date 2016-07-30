7fcea75f8f74532ce6aca75302ee44565f74f037
Pascal Bugnion <pascal@bugnion.org>
2016-07-30 07:58:41 +0100

    Fix datasets tests and remove dead tests

--- gmaps/datasets/tests/test_datasets.py
+++ gmaps/datasets/tests/test_datasets.py
@@ -10,10 +10,10 @@ class TestDatasets(unittest.TestCase):
 
 	def test_load_datasets(self):
 		data = datasets.load_dataset('taxi_rides')
-		assert data[0] == [37.782551, -122.445368]
+		assert data[0] == (37.782551, -122.445368)
 		assert len(data) == 500
 
 	def test_load_datasets_magnitude(self):
 		data = datasets.load_dataset('earthquakes')
-		assert data[0] == [65.1933, -149.0725, 1.7]
+		assert data[0] == (65.1933, -149.0725, 1.7)
 		assert len(data) == 8604
