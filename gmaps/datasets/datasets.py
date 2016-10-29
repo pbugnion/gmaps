093c62679417404594411d4cd392cb9e6d81845f
Pascal Bugnion <pascal@bugnion.org>
2016-10-29 15:57:07 +0100

    Ability to load non-float columns in datasets
    Prior to this commit, the columsn in datasets were all expected to be
    floats. Now that the datasets can be richer than just columns of
    latitudes and longitudes, we need to support more complex heterogeneous
    types.

--- gmaps/datasets/datasets.py
+++ gmaps/datasets/datasets.py
@@ -22,43 +22,49 @@ METADATA = {
     "taxi_rides": {
         "url": "https://s3-eu-west-1.amazonaws.com/jupyter-gmaps-examples/taxi_data.csv",  # noqa
         "description": "Taxi pickup location data in San Francisco",
-        "headers": ["latitude", "longitude"]
+        "headers": ["latitude", "longitude"],
+        "types": [float, float]
     },
     "earthquakes": {
         "url": "https://s3-eu-west-1.amazonaws.com/jupyter-gmaps-examples/earthquakes.csv",  # noqa
         "description": ("All recorded earthquakes in 30 days "
                         "starting on 12th November 2014"),
-        "headers": ["latitude", "longitude", "magnitude"]
+        "headers": ["latitude", "longitude", "magnitude"],
+        "types": [float, float, float]
     },
     "acled_africa": {
         "url": "https://s3-eu-west-1.amazonaws.com/jupyter-gmaps-examples/acled_africa.csv",  # noqa
         "description": "Recorded incidents of political violence in Africa",
         "source": "http://www.acleddata.com",
-        "headers": ["latitude", "longitude"]
+        "headers": ["latitude", "longitude"],
+        "types": [float, float]
     },
     "nuclear_plants": {
         "url": "https://s3-eu-west-1.amazonaws.com/jupyter-gmaps-examples/nuclear_power_plants.csv",  # noqa
         "description": "All nuclear power plants worldwide",
         "source": "IAEA (https://www.iaea.org/pris/)",
-        "headers": ["latitude", "longitude"]
+        "headers": ["latitude", "longitude"],
+        "types": [float, float]
     },
-    "starbucks_uk": {
-        "url": "https://s3-eu-west-1.amazonaws.com/jupyter-gmaps-examples/starbucks_uk.csv",  # noqa
-        "description": "All Starbucks in the UK (September 2016)",
-        "source": "http://ratings.food.gov.uk"
-    },
-    "kfc_uk": {
-        "url": "https://s3-eu-west-1.amazonaws.com/jupyter-gmaps-examples/kfc_uk.csv",  # noqa
-        "description": "All KFC restaurants in the UK (September 2016)",
-        "source": "http://ratings.food.gov.uk"
+    "starbucks_kfc_uk": {
+        "url": "https://s3-eu-west-1.amazonaws.com/jupyter-gmaps-examples/starbucks_kfc_uk.csv",  # noqa
+        "description": "All Starbucks and KFCs in the UK (September 2016)",
+        "source": "http://ratings.food.gov.uk",
+        "headers": ["latitude", "longitude", "chain_name"],
+        "types": [float, float, str]
     }
 }
 
 
-def _read_rows(f):
+def _read_rows(f, column_types):
     f.readline()  # skip header line
     reader = csv.reader(codecs.iterdecode(f, "utf-8"))
-    rows = [tuple(map(float, row)) for row in reader]
+    rows = []
+    for row in reader:
+        typed_row = [
+            column_type(cell) for column_type, cell in zip(column_types, row)
+        ]
+        rows.append(typed_row)
     return rows
 
 
@@ -93,8 +99,9 @@ def load_dataset(dataset_name):
     Fetch a dataset, returning an array of tuples.
     """
     url = METADATA[dataset_name]["url"]
+    column_types = METADATA[dataset_name]["types"]
     f = urlopen(url)
-    data = _read_rows(f)
+    data = _read_rows(f, column_types)
     f.close()
     return data
 
