28d909d61c365a4c16ebeee75550ea1ac2b42767
bruno_messias <messias.physics@gmail.com>
2016-12-17 20:47:20 -0200

    Change variable name info_html  to info_box_content

--- gmaps/tests/test_marker.py
+++ gmaps/tests/test_marker.py
@@ -11,28 +11,28 @@ class MarkerLayer(unittest.TestCase):
 
     def test_hover_text_atomic(self):
         marker_options = _marker_layer_options(
-            self.locations, hover_text="test-text", label="", info_html="")
+            self.locations, hover_text="test-text", label="", info_box_content="")
         for options in marker_options:
             assert options["hover_text"] == "test-text"
 
     def test_hover_text_lists(self):
         marker_options = _marker_layer_options(
-            self.locations, hover_text=["t1", "t2"], label="", info_html="")
+            self.locations, hover_text=["t1", "t2"], label="", info_box_content="")
         hover_texts = [options["hover_text"] for options in marker_options]
         assert tuple(hover_texts) == ("t1", "t2")
 
     def test_infobox_text_atomic(self):
         marker_options = _marker_layer_options(
-            self.locations, info_html="<h3>test-html-infobox</h3>", label="",
+            self.locations, info_box_content="<h3>test-html-infobox</h3>", label="",
             hover_text="")
         for options in marker_options:
-            assert options["info_html"] == "<h3>test-html-infobox</h3>"
+            assert options["info_box_content"] == "<h3>test-html-infobox</h3>"
 
     def test_infobox_text_lists(self):
         marker_options = _marker_layer_options(
-            self.locations, info_html=["<h1>h1</h1>", "<h2>h2</h2>"], label="",
+            self.locations, info_box_content=["<h1>h1</h1>", "<h2>h2</h2>"], label="",
             hover_text="")
-        infos_html = [options["info_html"] for options in marker_options]
+        infos_html = [options["info_box_content"] for options in marker_options]
         assert tuple(infos_html) == ("<h1>h1</h1>", "<h2>h2</h2>")
 
 
@@ -42,7 +42,7 @@ class SymbolLayer(unittest.TestCase):
         self.locations = [(-5.0, 5.0), (10.0, 10.0)]
         self.kwargs = {
             "hover_text": "", "fill_color": "red", "scale": 5,
-            "fill_opacity": 1.0, "stroke_opacity": 1.0, "info_html": ""
+            "fill_opacity": 1.0, "stroke_opacity": 1.0, "info_box_content": ""
         }
 
     def test_stroke_color_atomic_text(self):
