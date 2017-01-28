94ccedf652400884ef5480ae036729abdf4115e4
Pascal Bugnion <pascal@bugnion.org>
2017-01-28 09:34:54 +0000

    Docstring for directions_layer factory function

--- gmaps/directions.py
+++ gmaps/directions.py
@@ -99,5 +99,39 @@ def _directions_options(start, end, waypoints):
 
 
 def directions_layer(start, end, waypoints=None):
+    """
+    Create a directions layer.
+
+    Add this layer to a ``Map`` instance to draw directions on the map.
+    Currently, directions are limited to ``DRIVING`` directions.
+
+    :Examples:
+
+    >>> m = gmaps.Map()
+    >>> start = (46.2, 6.1)
+    >>> end = (47.4, 8.5)
+    >>> directions = gmaps.directions_layer(start, end)
+    >>> m.add_layer(directions)
+    >>> m
+
+    You can also add waypoints on the route:
+
+    >>> waypoints = [(46.4, 6.9), (46.9, 8.0)]
+    >>> directions = gmaps.directions_layer(start, end, waypoints=waypoints)
+
+    :param start:
+        (Latitude, longitude) pair denoting the start of the journey.
+    :type start: 2-element tuple
+
+    :param end:
+        (Latitude, longitude) pair denoting the end of the journey.
+    :type end: 2-element tuple
+
+    :param waypoints:
+        Iterable of (latitude, longitude) pair denoting waypoints.
+        Google maps imposes a limitation on the total number of waypoints.
+        This limit is currently 23.
+    :type waypoints: List of 2-element tuples, optional
+    """
     widget_args = _directions_options(start, end, waypoints)
     return Directions(**widget_args)
