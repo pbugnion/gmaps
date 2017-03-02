4927e3c25b96957c72368b6986680aa6ea6cc4bd
Pascal Bugnion <pascal@bugnion.org>
2017-03-02 06:50:25 +0000

    Edit tags for Python 3.6 support

--- setup.py
+++ setup.py
@@ -181,9 +181,8 @@ setup_args = {
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
 }
 
