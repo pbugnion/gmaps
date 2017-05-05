eb4aac29011223f278308f9d20f4e6118121d08a
Pascal Bugnion <pascal@bugnion.org>
2017-05-05 18:49:43 +0100

    Correct indentation for js components

--- js/src/figure.js
+++ js/src/figure.js
@@ -1,42 +1,44 @@
-import widgets from 'jupyter-js-widgets'
+ import widgets from 'jupyter-js-widgets'
 
 export const FigureModel = widgets.VBoxModel.extend({
-  defaults: {
-    ...widgets.DOMWidgetModel.prototype.defaults,
-    _model_name: "FigureModel",
-    _view_name: "FigureView",
-    _model_module: "jupyter-gmaps",
-    _view_module: "jupyter-gmaps",
-    children: [],
-    box_style: '',
-    _map: undefined,
-    _toolbar: undefined
-  }
+    defaults: {
+        ...widgets.DOMWidgetModel.prototype.defaults,
+        _model_name: "FigureModel",
+        _view_name: "FigureView",
+        _model_module: "jupyter-gmaps",
+        _view_module: "jupyter-gmaps",
+        children: [],
+        box_style: '',
+        _map: undefined,
+        _toolbar: undefined
+    }
 
 }, {
     serializers: _.extend({
-            children: {deserialize: widgets.unpack_models},
-            _map: {deserialize: widgets.unpack_models},
-            _toolbar: {deserialize: widgets.unpack_models},
+        children: {deserialize: widgets.unpack_models},
+        _map: {deserialize: widgets.unpack_models},
+        _toolbar: {deserialize: widgets.unpack_models},
     }, widgets.DOMWidgetModel.serializers)
 })
 
 export const FigureView = widgets.VBoxView.extend({
     initialize(parameters) {
-      FigureView.__super__.initialize.apply(this, arguments)
-      const toolbarModel = this.model.get("_toolbar");
-      if(toolbarModel) {
-        this.toolbarView =
-          this.add_child_model(this.model.get("_toolbar"))
-              .then(toolbarView => {
-                  toolbarView.registerSavePngCallback(() => this.savePng())
-                  return toolbarView;
-              })
-      }
-      else {
-        this.toolbarView = null;
-      }
-      this.mapView = this.add_child_model(this.model.get("_map"));
+        FigureView.__super__.initialize.apply(this, arguments)
+        const toolbarModel = this.model.get("_toolbar");
+        if(toolbarModel) {
+            this.toolbarView =
+                this.add_child_model(this.model.get("_toolbar"))
+                    .then(toolbarView => {
+                        toolbarView.registerSavePngCallback(
+                            () => this.savePng()
+                        )
+                        return toolbarView;
+                    })
+        }
+        else {
+            this.toolbarView = null;
+        }
+        this.mapView = this.add_child_model(this.model.get("_map"));
     },
 
     savePng() {
--- js/src/toolbar.js
+++ js/src/toolbar.js
@@ -1,68 +1,69 @@
 import widgets from 'jupyter-js-widgets'
 
 export const ToolbarModel = widgets.DOMWidgetModel.extend({
-  defaults: {
-    ...widgets.DOMWidgetModel.prototype.defaults,
-    _model_name: "ToolbarModel",
-    _view_name: "ToolbarView",
-    _model_module: "jupyter-gmaps",
-    _view_module: "jupyter-gmaps",
-  }
+    defaults: {
+        ...widgets.DOMWidgetModel.prototype.defaults,
+        _model_name: "ToolbarModel",
+        _view_name: "ToolbarView",
+        _model_module: "jupyter-gmaps",
+        _view_module: "jupyter-gmaps",
+    }
 });
 
 export const ToolbarView = widgets.DOMWidgetView.extend({
 
-  render() {
-    const $toolbar = $("<div />");
-    $toolbar
-      .addClass("toolbar-inner navbar-inner");
+    render() {
+        const $toolbar = $("<div />");
+        $toolbar
+            .addClass("toolbar-inner navbar-inner");
 
-    const $toolbarContainer = $("<div />")
-    $toolbarContainer
-      .addClass("container toolbar gmaps-toolbar-container");
+        const $toolbarContainer = $("<div />")
+        $toolbarContainer
+            .addClass("container toolbar gmaps-toolbar-container");
 
-    const $saveButton = $("<button />")
-    $saveButton
-      .addClass("btn btn-default gmaps-toolbar-button")
-      .attr("title", "Download the map as PNG")
-      .append("<i />")
-        .addClass("fa fa-download");
+        const $saveButton = $("<button />")
+        $saveButton
+            .addClass("btn btn-default gmaps-toolbar-button")
+            .attr("title", "Download the map as PNG")
+            .append("<i />")
+            .addClass("fa fa-download");
 
-    const $notificationArea = $("<span />");
-    $notificationArea
-      .addClass("gmaps-toolbar-notification-area");
+        const $notificationArea = $("<span />");
+        $notificationArea
+            .addClass("gmaps-toolbar-notification-area");
 
-    const $savingNotification = $("<button />")
-    $savingNotification
-      .addClass("notification_widget btn btn-xs navbar-btn warning gmaps-notification-widget")
-      .html("<span>Downloading</span>")
-      .hide();
+        const $savingNotification = $("<button />")
+        $savingNotification
+            .addClass("notification_widget btn btn-xs navbar-btn")
+            .addClass("warning gmaps-notification-widget")
+            .html("<span>Downloading</span>")
+            .hide();
 
-    $saveButton
-      .click((event) => {
-        event.preventDefault();
-        $saveButton.prop("disabled", true)
-        $savingNotification.show()
-        if (this.savePngCallback) {
-          this.savePngCallback().then(() => {
-            $saveButton.prop("disabled", false)
-            $savingNotification.hide();
-          });
-        };
-      })
+        $saveButton
+            .click((event) => {
+                event.preventDefault();
+                $saveButton.prop("disabled", true)
+                $savingNotification.show()
+                if (this.savePngCallback) {
+                    this.savePngCallback().then(() => {
+                        $saveButton.prop("disabled", false)
+                        $savingNotification.hide();
+                    });
+                };
+            })
 
-    $toolbarContainer
-      .append($saveButton)
-      .append($notificationArea);
-    $notificationArea.append($savingNotification);
-    $toolbar.append($toolbarContainer)
-    this.$el.append($toolbar)
+        $toolbarContainer
+            .append($saveButton)
+            .append($notificationArea);
+        $notificationArea.append($savingNotification);
+        $toolbar.append($toolbarContainer)
+        this.$el.append($toolbar)
 
-    this.update();
-  },
+        this.update();
+    },
 
-  registerSavePngCallback(callback) {
-      this.savePngCallback = callback;
-  }
+    registerSavePngCallback(callback) {
+        this.savePngCallback = callback;
+    }
 })
 
