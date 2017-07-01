3fed637b88c9bb7065c8fd3ba4bf1acce4b5f45e
Pascal Bugnion <pascal@asidatascience.com>
2017-07-01 07:57:10 +0000

    Use ES6 syntax for classes

--- js/src/Figure.js
+++ js/src/Figure.js
@@ -3,33 +3,34 @@ import _ from 'underscore';
 
 import widgets from 'jupyter-js-widgets';
 
-export const FigureModel = widgets.VBoxModel.extend({
-    defaults: {
-        ...widgets.DOMWidgetModel.prototype.defaults,
-        _model_name: "FigureModel",
-        _view_name: "FigureView",
-        _model_module: "jupyter-gmaps",
-        _view_module: "jupyter-gmaps",
-        children: [],
-        box_style: '',
-        _map: undefined,
-        _errors_box: undefined,
-        _toolbar: undefined
+export class FigureModel extends widgets.VBoxModel {
+    defaults() {
+        return {
+            ...super.defaults(),
+            _model_name: "FigureModel",
+            _view_name: "FigureView",
+            _model_module: "jupyter-gmaps",
+            _view_module: "jupyter-gmaps",
+            children: [],
+            box_style: '',
+            _map: undefined,
+            _errors_box: undefined,
+            _toolbar: undefined
+        }
     }
 
-}, {
-    serializers: {
+    static serializers = {
+        ...widgets.DOMWidgetModel.serializers,
         children: {deserialize: widgets.unpack_models},
         _map: {deserialize: widgets.unpack_models},
         _toolbar: {deserialize: widgets.unpack_models},
-        _errors_box: {deserialize: widgets.unpack_models},
-        ...widgets.DOMWidgetModel.serializers
+        _errors_box: {deserialize: widgets.unpack_models}
     }
-})
+}
 
-export const FigureView = widgets.VBoxView.extend({
+export class FigureView extends widgets.VBoxView {
     initialize(parameters) {
-        FigureView.__super__.initialize.apply(this, arguments)
+        super.initialize(parameters)
         const toolbarModel = this.model.get("_toolbar");
         if(toolbarModel) {
             this.toolbarView =
@@ -50,13 +51,13 @@ export const FigureView = widgets.VBoxView.extend({
                 this.add_child_model(this.model.get("_errors_box"));
         }
         this.mapView = this.add_child_model(this.model.get("_map"));
-    },
+    }
 
     savePng() {
         return this.mapView.then(view =>
             view.savePng().catch(e => this.addError(e))
         );
-    },
+    }
 
     addError(errorMessage) {
         console.log(`[Error]: ${errorMessage}`)
@@ -65,4 +66,4 @@ export const FigureView = widgets.VBoxView.extend({
             errorsBoxModel.addError(errorMessage);
         }
     }
-})
+}
