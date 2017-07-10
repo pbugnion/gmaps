149a4d83924fbebf1d97007a9f50d237bf7d044b
Pascal Bugnion <pascal@asidatascience.com>
2017-07-10 06:12:05 +0000

    Change directions layer test to test widget directly
    Prior to this commit, the tests tested a private method. If I remember
    correctly, this is because, in earlier versions of ipywidgets, widgets could
    not be instantiated outside of a notebook.
    
    Now we can just create the widget directly and verify that it gets given the
    correct options.

--- gmaps/tests/test_directions.py
+++ gmaps/tests/test_directions.py
@@ -2,7 +2,7 @@
 import unittest
 import pytest
 
-from ..directions import _directions_options
+from ..directions import directions_layer
 
 
 class DirectionsLayer(unittest.TestCase):
@@ -26,54 +26,43 @@ class DirectionsLayer(unittest.TestCase):
         return new_options
 
     def test_no_waypoints(self):
-        options = _directions_options(
-            self.start, self.end, waypoints=None,
-            **self._add_default_options()
-        )["data"]
-        assert options == self.data_array_no_waypoints
+        layer = directions_layer(self.start, self.end)
+        assert layer.data == self.data_array_no_waypoints
 
     def test_waypoints(self):
-        options = _directions_options(
-            self.start, self.end, self.waypoints,
-            **self._add_default_options()
-        )["data"]
-        assert options == self.data_array
+        layer = directions_layer(self.start, self.end, self.waypoints)
+        assert layer.data == self.data_array
 
     def test_no_waypoints_numpy_array(self):
         import numpy as np
-        options = _directions_options(
-            np.array(self.start), self.end, None,
-            **self._add_default_options()
-        )["data"]
-        assert options == self.data_array_no_waypoints
+        layer = directions_layer(np.array(self.start), self.end)
+        assert layer.data == self.data_array_no_waypoints
 
     def test_waypoints_numpy_array(self):
         import numpy as np
-        options = _directions_options(
-            np.array(self.start), self.end, np.array(self.waypoints),
-            **self._add_default_options()
-        )["data"]
-        assert options == self.data_array
+        layer = directions_layer(
+                np.array(self.start),
+                self.end,
+                np.array(self.waypoints)
+        )
+        assert layer.data == self.data_array
 
     def test_pandas_df(self):
         pd = pytest.importorskip("pandas")
         waypoints = pd.DataFrame.from_records(
             self.waypoints, columns=["latitude", "longitude"])
-        options = _directions_options(
-            self.start, self.end, waypoints,
-            **self._add_default_options()
-        )["data"]
-        assert options == self.data_array
+        layer = directions_layer(self.start, self.end, waypoints)
+        assert layer.data == self.data_array
 
     def test_boolean_options(self):
-        options = _directions_options(
+        layer = directions_layer(
             self.start, self.end, waypoints=None,
             avoid_ferries=True,
             avoid_highways=True,
             avoid_tolls=True,
             optimize_waypoints=True
         )
-        assert options["avoid_ferries"]
-        assert options["avoid_highways"]
-        assert options["avoid_tolls"]
-        assert options["optimize_waypoints"]
+        assert layer.avoid_ferries
+        assert layer.avoid_highways
+        assert layer.avoid_tolls
+        assert layer.optimize_waypoints
