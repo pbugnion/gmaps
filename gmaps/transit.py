a6201f600afd7d91fd1a739b18227a5ff2caaa06
Pascal Bugnion <pascal@asidatascience.com>
2017-07-31 07:02:04 +0000

    Add transit factory function

--- gmaps/transit.py
+++ gmaps/transit.py
@@ -8,3 +8,24 @@ class Transit(widgets.Widget):
     _model_name = Unicode('TransitLayerModel').tag(sync=True)
     _model_module = Unicode('jupyter-gmaps').tag(sync=True)
     has_bounds = False
+
+
+def transit_layer():
+    """
+    Transit layer.
+
+    Adds information about public transport lines to the
+    map. This only affects region for which Google has
+    `public transport information
+    <https://www.google.com/landing/transit/cities/index.html>`_.
+
+    :Examples:
+
+    ::
+
+        # map centered on London
+        >>> fig = gmaps.figure(center=(51.5, -0.2), zoom_level=11)
+        >>> fig.add_layer(gmaps.transit_layer())
+        >>> fig
+    """
+    return Transit()
