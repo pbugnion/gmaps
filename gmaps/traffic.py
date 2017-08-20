9e994c2a48f61a6cdebcbfc6177c68ac1bad2281
Pascal Bugnion <pascal@asidatascience.com>
2017-08-20 15:19:11 +0000

    Docstrings and API documentation for traffic layer.

--- gmaps/traffic.py
+++ gmaps/traffic.py
@@ -8,6 +8,22 @@ from .maps import GMapsWidgetMixin
 class Traffic(GMapsWidgetMixin, widgets.Widget):
     """
     Traffic layer
+
+    Add this to a :class:`gmaps.Map` or a :class:`gmaps.Figure`
+    instance to add traffic information to the map, where
+    supported.
+
+    You should not instantiate this directly. Instead,
+    use the :func:`gmaps.traffic_layer` factory function.
+
+    :Examples:
+
+    ::
+
+        # map centered on London
+        >>> fig = gmaps.figure(center=(51.5, -0.2), zoom_level=11)
+        >>> fig.add_layer(gmaps.traffic_layer())
+        >>> fig
     """
     _view_name = Unicode('TrafficLayerView').tag(sync=True)
     _model_name = Unicode('TrafficLayerModel').tag(sync=True)
@@ -16,6 +32,23 @@ class Traffic(GMapsWidgetMixin, widgets.Widget):
 
 def traffic_layer():
     """
-    Traffic layer
+    Traffic layer.
+
+    Adds information about the current state of traffic
+    to the map. This layer only works at sufficiently high
+    zoom levels, and for regions for which Google Maps
+    has traffic information.
+
+    :returns:
+        A :class:`gmaps.Traffic` widget.
+
+    :Examples:
+
+    ::
+
+        # map centered on London
+        >>> fig = gmaps.figure(center=(51.5, -0.2), zoom_level=11)
+        >>> fig.add_layer(gmaps.traffic_layer())
+        >>> fig
     """
     return Traffic()
