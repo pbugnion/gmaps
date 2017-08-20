42dd7b9c42a30148edd207e54c599b12bb942f45
Pascal Bugnion <pascal@asidatascience.com>
2017-08-20 14:06:06 +0000

    Bump to version 0.6.0-alpha4.
    This release is fully functional with ipywidgets 7.0.0.

--- gmaps/_version.py
+++ gmaps/_version.py
@@ -2,7 +2,7 @@
 # This file is generated programatically.
 
 # Version of the Python package
-__version__ = '0.6.0-alpha2'
+__version__ = '0.6.0-alpha4'
 
 # Version of the JS client. This must match the version field in package.json.
-CLIENT_VERSION = '0.6.0-alpha2'
+CLIENT_VERSION = '0.6.0-alpha4'
