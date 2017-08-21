23c75132c2cb2f5e49aefd6b99b9fcb61f259a5a
Pascal Bugnion <pascal@asidatascience.com>
2017-08-21 06:48:10 +0000

    Document auto_refresh parameter

--- gmaps/traffic.py
+++ gmaps/traffic.py
@@ -24,6 +24,11 @@ class Traffic(GMapsWidgetMixin, widgets.Widget):
         >>> fig = gmaps.figure(center=(51.5, -0.2), zoom_level=11)
         >>> fig.add_layer(gmaps.traffic_layer())
         >>> fig
+
+    :param auto_refresh:
+        Whether the traffic layer refreshes with updated
+        information automatically. This is true by default.
+    :type auto_refresh: bool, optional
     """
     _view_name = Unicode('TrafficLayerView').tag(sync=True)
     _model_name = Unicode('TrafficLayerModel').tag(sync=True)
@@ -39,6 +44,11 @@ def traffic_layer(auto_refresh=True):
     to the map. This layer only works at sufficiently high
     zoom levels, and for regions for which Google Maps
     has traffic information.
+
+    :param auto_refresh:
+        Whether the traffic layer refreshes with updated
+        information automatically. This is true by default.
+    :type auto_refresh: bool, optional
 
     :returns:
         A :class:`gmaps.Traffic` widget.
