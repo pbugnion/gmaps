5eb95e38ccff264d4df0669f32e92f384b689e20
Pascal Bugnion <pascal@bugnion.org>
2017-09-05 23:03:05 +0100

    Ability to add control elements to toolbar
    This is useful fow when layers need to add their own drawing elements

--- gmaps/drawing.py
+++ gmaps/drawing.py
@@ -1,10 +1,36 @@
 
 import ipywidgets as widgets
 
-from traitlets import Unicode, List
+from traitlets import Unicode, List, Enum, Instance, Bool, HasTraits
 
 from .maps import GMapsWidgetMixin
 from .marker import Marker
+
+
+ALLOWED_DRAWING_MODES = {'DISABLED', 'MARKER'}
+DEFAULT_DRAWING_MODE = 'MARKER'
+
+
+def serialize_drawing_layer_options(options, manager):
+    return {'mode': options.mode}
+
+
+class DrawingLayerOptions(HasTraits):
+    mode = Enum(
+        ALLOWED_DRAWING_MODES, 
+        default_value=DEFAULT_DRAWING_MODE
+    )
+
+
+class DrawingControls(GMapsWidgetMixin, widgets.DOMWidget):
+    _model_name = Unicode('DrawingControlsModel').tag(sync=True)
+    _view_name = Unicode('DrawingControlsView').tag(sync=True)
+    show_controls = Bool(default_value=True, allow_none=False).tag(
+        sync=True)
+    options = Instance(
+        DrawingLayerOptions,
+        allow_none=True
+    ).tag(sync=True, to_json=serialize_drawing_layer_options)
 
 
 class Drawing(GMapsWidgetMixin, widgets.Widget):
@@ -12,6 +38,13 @@ class Drawing(GMapsWidgetMixin, widgets.Widget):
     _view_name = Unicode('DrawingLayerView').tag(sync=True)
     _model_name = Unicode('DrawingLayerModel').tag(sync=True)
     overlays = List().tag(sync=True, **widgets.widget_serialization)
+    options = Instance(
+        DrawingLayerOptions,
+        allow_none=False,
+        default=DrawingLayerOptions()
+    ).tag(sync=True, **widgets.widget_serialization)
+    toolbar_controls = Instance(DrawingControls).tag(
+        sync=True, **widgets.widget_serialization)
 
     def __init__(self, **kwargs):
         super(Drawing, self).__init__(**kwargs)
--- gmaps/toolbar.py
+++ gmaps/toolbar.py
@@ -1,7 +1,7 @@
 
 import ipywidgets as widgets
 
-from traitlets import Unicode
+from traitlets import Unicode, List
 
 from .maps import GMapsWidgetMixin
 
@@ -9,3 +9,8 @@ from .maps import GMapsWidgetMixin
 class Toolbar(GMapsWidgetMixin, widgets.DOMWidget):
     _view_name = Unicode("ToolbarView").tag(sync=True)
     _model_name = Unicode("ToolbarModel").tag(sync=True)
+    layer_controls = List().tag(
+        sync=True, **widgets.widget_serialization)
+
+    def add_controls(self, controls):
+        self.layer_controls = list(self.layer_controls) + [controls]
