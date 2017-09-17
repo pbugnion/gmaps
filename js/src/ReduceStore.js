d0e3cd82614d796e3457a791ed5b76eb6ced6638
Pascal Bugnion <pascal@bugnion.org>
2017-09-17 16:22:23 +0100

    Fix indentation issues in ReduceStore

--- js/src/ReduceStore.js
+++ js/src/ReduceStore.js
@@ -24,16 +24,16 @@ export class Store extends BaseStore {
         console.assert(endingState !== undefined);
 
         if (!this.areEqual(startingState, endingState)) {
-        this._state = endingState;
+            this._state = endingState;
 
-        // `__emitChange()` sets `this.__changed` to true and then the actual
-        // change will be fired from the emitter at the end of the dispatch, this
-        // is required in order to support methods like `hasChanged()`
-        this.__emitChange();
+            // `__emitChange()` sets `this.__changed` to true and then the actual
+            // change will be fired from the emitter at the end of the dispatch, this
+            // is required in order to support methods like `hasChanged()`
+            this.__emitChange();
         }
 
         if (this.__changed) {
-        this.__emitter.emit(this.__changeEvent);
+            this.__emitter.emit(this.__changeEvent);
         }
     }
 }
