b55ac06b83dc9312a4346bfa747be8eaf2786095
Pascal Bugnion <pascal@bugnion.org>
2017-11-11 14:38:40 +0000

    Back to dev

--- gmaps/_version.py
+++ gmaps/_version.py
@@ -2,7 +2,7 @@
 # This file is generated programatically.
 
 # Version of the Python package
-__version__ = '0.7.0'
+__version__ = '0.7.1-dev'
 
 # Version of the JS client. This must match the version field in package.json.
-CLIENT_VERSION = '0.7.0'
+CLIENT_VERSION = '0.7.1-dev'
