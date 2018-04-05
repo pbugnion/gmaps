1b2b273b63d3bf05eb54a2e83cb66f4e04e26e40
Pascal Bugnion <pascal@bugnion.org>
2018-04-05 07:34:36 +0100

    Bump version to 0.7.4

--- gmaps/_version.py
+++ gmaps/_version.py
@@ -2,7 +2,7 @@
 # This file is generated programatically.
 
 # Version of the Python package
-__version__ = '0.7.4-dev'
+__version__ = '0.7.4'
 
 # Version of the JS client. This must match the version field in package.json.
-CLIENT_VERSION = '0.7.4-dev'
+CLIENT_VERSION = '0.7.4'
