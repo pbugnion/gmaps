3a34b09981d482e872963a40cc64d42e485c539d
Pascal Bugnion <pascal@bugnion.org>
2019-01-14 07:36:21 +0000

    Update to webpack 4

--- js/webpack.config.js
+++ js/webpack.config.js
@@ -2,10 +2,10 @@ var version = require('./package.json').version;
 
 // Custom webpack loaders are generally the same for all webpack bundles, hence
 // stored in a separate local variable.
-var loaders = [
+var rules = [
     { test: /\.css$/, loader: 'style-loader!css-loader' },
     { test: /\.less$/, loader: 'style-loader!css-loader!less-loader' },
-    { test: /\.json$/, loader: 'json-loader' },
+    // { test: /\.json$/, loader: 'json-loader', exclude: /node_modules/ },
     { test: /\.js$/, loader: 'babel-loader', query: {presets: ['es2015', 'stage-0']}, exclude: /node_modules/ },
     { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=application/octet-stream' },
     { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=image/svg+xml' },
@@ -41,7 +41,7 @@ module.exports = [
             libraryTarget: 'amd'
         },
         module: {
-            loaders: loaders
+            rules: rules
         },
         externals: ['@jupyter-widgets/base', '../dist/index']
     },
@@ -59,7 +59,7 @@ module.exports = [
         },
         devtool: 'source-map',
         module: {
-            loaders: loaders
+            rules: rules
         },
         externals: ['@jupyter-widgets/base', '@jupyter-widgets/controls']
     },
@@ -86,7 +86,7 @@ module.exports = [
         },
         devtool: 'source-map',
         module: {
-            loaders: loaders
+            rules: rules
         },
         externals: ['@jupyter-widgets/base', '@jupyter-widgets/controls']
     }
