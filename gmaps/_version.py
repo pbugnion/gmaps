6cda02dde8519181738f75902ec04b9ed11f95b1
Pascal Bugnion <pascal@asidatascience.com>
2019-01-25 18:11:26 +0000

    Back to dev

--- gmaps/_version.py
+++ gmaps/_version.py
@@ -2,7 +2,7 @@
 # This file is generated programatically.
 
 # Version of the Python package
-__version__ = '0.8.4'
+__version__ = '0.8.5-dev'
 
 # Version of the JS client. This must match the version field in package.json.
-CLIENT_VERSION = '0.8.4'
+CLIENT_VERSION = '0.8.5-dev'
